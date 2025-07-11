16be8d8493e5764e716577f6f608f62e92b54f13
Utkarsh Gaikwad <109385199+utkarshg1@users.noreply.github.com>
2025-07-11 09:24:28 +0000

    updated

--- app.py
+++ app.py
@@ -1,1 +1,30 @@
-# Write your code here
+import joblib
+import streamlit as st
+from utils import predict_species
+
+# Load the joblib model
+model = joblib.load("notebook/iris_model.joblib")
+
+# Streamlit app
+st.set_page_config(page_title="Iris Project")
+
+# Write the title
+st.title("Iris End to End Project")
+st.subheader("by Utkarsh Gaikwad")
+
+# Input from user
+sep_len = st.number_input("Sepal Length : ", min_value=0.00, step=0.01)
+sep_wid = st.number_input("Sepal Width : ", min_value=0.00, step=0.01)
+pet_len = st.number_input("Petal Length : ", min_value=0.00, step=0.01)
+pet_wid = st.number_input("Petal Width : ", min_value=0.00, step=0.01)
+
+# Create a predict button
+button = st.button("Predict", type="primary")
+
+# if button pressed
+if button:
+    st.subheader("Results : ")
+    pred, prob = predict_species(model, sep_len, sep_wid, pet_len, pet_wid)
+    st.subheader(f"Predicted Species : {pred}")
+    st.subheader(f"Probabilities")
+    st.dataframe(prob)
--- utils.py
+++ utils.py
@@ -1,1 +1,25 @@
-# Write your code here
+# Predict the results
+import pandas as pd
+from sklearn.pipeline import Pipeline
+
+
+def predict_species(
+        model: Pipeline, 
+        sep_len: float,
+        sep_wid:float,
+        pet_len: float,
+        pet_wid: float):
+    xnew = pd.DataFrame(
+        [
+            {
+                "sepal_length": sep_len,
+                "sepal_width": sep_wid,
+                "petal_length": pet_len,
+                "petal_width": pet_wid 
+            }
+        ]
+    )
+    pred = model.predict(xnew)
+    probs = model.predict_proba(xnew)
+    probs_df = pd.DataFrame(probs, columns=model.classes_)
+    return pred[0], probs_df.round(4)
